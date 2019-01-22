3af8c488700d8192ecb0f9c8eee8e0ea0703d80e
hechtermach <36723756+hechtermach@users.noreply.github.com>
2019-01-22 17:03:53 -0500

    Add files via upload

--- chuan_shu.py
+++ chuan_shu.py
@@ -2,14 +2,11 @@ import itertools
 list_step=['R','L','U','D']
 list_all=[]
 list_path=[]
+list_location=[]
+list_feasible_path=[]
+
 def generate_un_list(s):
-    count_r = s.count('R')
-    count_l= s.count('L')
-    count_u = s.count('U')
-    count_d = s.count('D')
     count_un=s.count('?')
-    distance_x=count_r-count_l
-    distance_y=count_u-count_d
     if count_un==1:
         return list_step
     elif count_un==2:
@@ -22,16 +19,43 @@ def generate_un_list(s):
 def generate_all_list(s):
     global list_path
     count_un=s.count('?')
-    generate_index=0
     un_list=generate_un_list(s)
     for combo in un_list:
         ss=s      
         for i in combo:
             ss=ss.replace('?',i,1)    
             list_all.append(ss)
-    for i in range(4^(count_un)+1):
-        list_path.append(list_all[count_un*i])
-    return list_path
-#    return list_all
-ttttt=generate_all_list('URL???DDDDU')
-print (ttttt)  
+    for i in range(4**(count_un)):
+        list_path.append(list_all[count_un*i+count_un-1])
+    return list_path 
+
+def check_path_right(s,w,h):
+    paths=generate_all_list(s)
+    for path in paths:
+        x=0
+        y=0
+        list_location=[(x,y)]
+        for step_each in path:
+            if step_each=='R':
+                x=x+1
+            elif step_each=='L':
+                x=x-1
+            elif step_each=='U':
+                y=y+1
+            else:
+                y=y-1
+            if (x,y) not in list_location:
+                list_location.append((x,y))
+            else:
+                break
+        if (x,y)==(w-1,h-1):
+            list_feasible_path.append(path)
+    if list_feasible_path==[]:
+        return 'null'
+    return list_feasible_path
+
+def CompletePath(s,w,h):
+    output=check_path_right(s,w,h) 
+    print (output) 
+
+CompletePath('?RDRR?UUUR',5,5)         
